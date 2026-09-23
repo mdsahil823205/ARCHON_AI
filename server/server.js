0b0364bd201a66873818f9e0511284a71cc4e0cb
mdsahil823205 <haider823205@gmail.com>
2026-09-23 14:47:45 +0530

    add dist folder in server using of npm run build

--- server/server.js
+++ server/server.js
@@ -7,10 +7,16 @@ import cors from "cors";
 import userRouter from "./src/routes/user.route.js";
 import websiteRouter from "./src/routes/website.route.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
+
+// __dirname for ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(express.json());
@@ -26,19 +32,33 @@ app.use(
   })
 );
 
-// Database Connection
+// Database
 dbConnect();
 
-// Routes
+// API Routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/website", websiteRouter);
+
+// Serve React frontend
+app.use(express.static(path.join(__dirname, "dist")));
+
+// React Router fallback
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(
+      path.join(__dirname, "dist", "index.html")
+    );
+  }
+
+  next();
+});
 
 // Server
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 export default app;
